b2a01d2aaf3c46e1109cbc0914e1c17c42069f41
Petone1488 <main.mailbox@bk.ru>
2023-03-03 17:51:14 +0300

    test

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Console.WriteLine("Hello, World!");
+int a,b;
+a=2;
+b=1;
+
+Console.WriteLine(a);
+
+System.Console.WriteLine(b);
+
+bool variable = true;
+bool variable2 = false;
+
+System.Console.WriteLine(variable);
+System.Console.WriteLine(variable2);
