b1323a245049df135bd581a288a1ed5eedb6eb61
Petone1488 <main.mailbox@bk.ru>
2023-03-09 13:46:46 +0300

    save2

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Console.WriteLine("Hello, World!");
+
 // int a,b;
 // a=2;
 // b=1;
-
-// Console.WriteLine(a);
+// Console.WriteLine(a+b);
 
 // System.Console.WriteLine(b);
 
@@ -13,7 +13,35 @@
 // System.Console.WriteLine(variable);
 // System.Console.WriteLine(variable2);
 
-string ;
-A = Console.ReadLine();
-Console.WriteLine("Моё имя " + A);
+// string names;
+// names = Console.ReadLine();
+// Console.WriteLine("Моё имя " + names);
+
+// string str;
+// int a, b;
+// Console.WriteLine("Введите первое число ");
+// str = Console.ReadLine();
+// a = Convert.ToInt32(str);
+// Console.WriteLine("Введите второе число ");
+// str = Console.ReadLine();
+// b = Convert.ToInt32(str);
+// int result = a + b;
+// Console.WriteLine("Сумма чисел = " + result);
+
+int numbers;
+numbers = 2;
+int numbers2;
+numbers2 = 6;
+int numbers3 = 2;
+int result;
+result = ((numbers + numbers2)/numbers3)/2;
+Console.WriteLine("Результат деления = " + result);
+
+
+
+
+
+
+
+
 
